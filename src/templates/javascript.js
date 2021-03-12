9fac1cc20938ee58b86819b6fbe2120229278c5f
Joel Arvidsson <joel.arvidsson@klarna.com>
2021-03-12 14:34:04 +0100

    Implement JS templates and file output

---
+++ src/templates/javascript.js
@@ -0,0 +1,43 @@
+const NATIVE_HEADER = `import { PlatformColor } from 'react-native';`;
+
+const generateIos = (colors) =>
+  NATIVE_HEADER +
+  '\n\n' +
+  colors
+    .map(
+      (color) => `export const ${color.name} = PlatformColor('${color.name}');`
+    )
+    .join('\n');
+
+const generateAndroid = (colors) =>
+  NATIVE_HEADER +
+  '\n\n' +
+  colors
+    .map(
+      (color) =>
+        `export const ${color.name} = PlatformColor('@android:color/${color.name}');`
+    )
+    .join('\n');
+
+const generateCss = (colors) =>
+  colors
+    .map(
+      (color) => `export const ${color.name} = 'var(--color-${color.name})';`
+    )
+    .join('\n');
+
+const generateTypes = (colors) =>
+  colors.map((color) => `export const ${color.name}: unknown;`).join('\n');
+
+module.exports = function generateJavaScript(colors, config) {
+  const platforms = [].filter(Boolean);
+  const typescript = Boolean(config.javascript && config.javascript.typescript);
+  const extension = typescript ? '.ts' : '.js';
+  return [
+    config.ios && [`index.ios${extension}`, generateIos(colors)],
+    config.android && [`index.android${extension}`, generateAndroid(colors)],
+    config.css && [`index${extension}`, generateCss(colors)],
+    typescript &&
+      (config.ios || config.android) && ['index.d.ts', generateTypes(colors)],
+  ].filter(Boolean);
+};
