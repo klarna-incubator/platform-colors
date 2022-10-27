f8f66290c5c34f93c52824cbbb201d14a3d651d4
Joel Arvidsson <joel.arvidsson@klarna.com>
2022-10-27 11:15:31 +0200

    Upgrade example project to react-native 0.70

--- examples/ColorViewerApp/colors/index.ts
+++ examples/ColorViewerApp/colors/index.ts
@@ -1,4 +1,4 @@
-export const background = 'var(--rnpc-background)'
-export const text = 'var(--rnpc-text)'
-export const accent = 'var(--rnpc-accent)'
-export const contrasted = 'var(--rnpc-contrasted)'
+export const background = 'var(--rnpc-background)';
+export const text = 'var(--rnpc-text)';
+export const accent = 'var(--rnpc-accent)';
+export const contrasted = 'var(--rnpc-contrasted)';
